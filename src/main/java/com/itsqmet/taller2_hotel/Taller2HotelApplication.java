8bc1c6abc1092cdeb05443d774d14ecec4c4b5a8
Tu Nombre <tu-email@example.com>
2026-06-23 11:14:57 -0500

    Inicio de proyecto

---
+++ src/main/java/com/itsqmet/taller2_hotel/Taller2HotelApplication.java
@@ -0,0 +1,13 @@
+package com.itsqmet.taller2_hotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller2HotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller2HotelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/itsqmet/taller2_hotel/Taller2HotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.taller2_hotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller2HotelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
