36504785cbdc0b0190dfd29250b535db991a4885
Mateo <mateocarpio4@gmail.com>
2026-06-23 20:08:45 -0500

    primer cambio

--- src/main/java/com/itsqmet/taller2_hotel/model/Huesped.java
+++ src/main/java/com/itsqmet/taller2_hotel/model/Huesped.java
@@ -1,4 +1,108 @@
 package com.itsqmet.taller2_hotel.model;
 
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+import java.util.List;
+
+@Entity
+@Table(name = "huespedes")
 public class Huesped {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank(message = "El nombre no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El nombre debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false)
+    private String nombre;
+
+    @NotBlank(message = "El apellido no puede estar vacío")
+    @Size(min = 3, max = 50, message = "El apellido debe tener entre 3 y 50 caracteres")
+    @Column(nullable = false)
+    private String apellido;
+
+    @NotBlank(message = "La cédula no puede estar vacía")
+    @Column(nullable = false, unique = true)
+    private String cedula;
+
+    @Email(message = "Ingrese un correo válido")
+    @Column(unique = true)
+    private String correo;
+
+    @NotBlank(message = "El teléfono no puede estar vacío")
+    private String telefono;
+
+    // RELACIÓN 1:1
+
+
+    // RELACIÓN 1:N INVERSA
+
+
+    public Huesped() {
+
+    }
+
+    public Huesped(Long id, String nombre, String apellido, String cedula, String correo, String telefono) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.cedula = cedula;
+        this.correo = correo;
+        this.telefono = telefono;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getCedula() {
+        return cedula;
+    }
+
+    public void setCedula(String cedula) {
+        this.cedula = cedula;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+
 }
